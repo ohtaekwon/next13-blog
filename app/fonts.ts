347379e4850a642519108ed5ac45b6cd5331b8be
ohtaekwon <ohtk1119@gmail.com>
2023-05-11 17:39:42 +0900

    Feat Setting up the initial project

---
+++ app/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from "next/font/google";
+import { Open_Sans } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+const openSans = Open_Sans({ subsets: ["latin"] });
+
+export default { inter, openSans };
